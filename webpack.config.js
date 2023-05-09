a870ee2e209798c46e87f52cb4ce410b41b7dbff
Justin Naling <justinbnaling@gmail.com>
2023-05-08 20:22:52 -0500

    first commit with npm and webpack loaded

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    module: {
+        rules: [
+            // {
+            //     test: require.resolve('./src/myObject.js'),
+            //     loader: 'expose-loader',
+            //     options: {
+            //       exposes: {
+            //         globalName: 'myObject',
+            //         moduleLocalName: 'myObject',
+            //       },
+            //       override: true,
+            //     },
+            //   }            
+        ]
+    }
+}
